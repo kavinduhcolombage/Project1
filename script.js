67c2435c9e7abc52e89af0a32d3f49f7894416f2
Kavindu Hansana <hansana2024@gmail.com>
2024-06-19 23:11:53 +0530

    add feautures

--- script.js
+++ script.js
@@ -1,14 +1,3 @@
-let num1;
-let num2;
-
-function getvalue(){
-    num1 = document.getElementById("num1").value;
-    num2 = document.getElementById("num2").value;
-}
-
-
-function sum(){
-    result = num1+num2;
-    document.getElementById("output").textContent = result;
-  }
-
+document.getElementById('scrollButton').addEventListener('click', function() {
+    document.getElementById('targetParagraph').scrollIntoView({ behavior: 'smooth' });
+});
