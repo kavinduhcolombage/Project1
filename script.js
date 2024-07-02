b69ffe2ef48778ec01412f530ee26813c685d2da
Kavindu Hansana <hansana2024@gmail.com>
2024-07-02 18:13:22 +0530

    form update

--- script.js
+++ script.js
@@ -2,19 +2,23 @@ function submissionSuccess(){
     let name = document.forms["form"]["name"].value;
     let email = document.forms["form"]["email"].value;
     let message = document.forms["form"]["message"].value;
-    if(name == "" || email == ""){
-        alert('Please fill field!');
-        return false;
-    }
-    if (!email.includes("@") || !email.includes(".")) {
-        alert("Please enter a valid email address");
-        return false;
-    }
-    if(message == ""){
-        alert('Enter your message');
-        return false;
-    }
+    let phonenumber = document.forms["form"]["phonenumber"].value;
 
-    alert('Request Sent Success!');
+    if(name == "" || email == "" || phonenumber == "" || message == ""){
+        alert('Please fill field!');
+        return;
+    }else{
+        if (!email.includes("@") || !email.includes(".")) {
+            alert("Please enter a valid email address");
+            return;
+        }else{
+            alert('Request Sent Success!');
+        }
+    }
+    
+    
+    
+
+  
 }
 
